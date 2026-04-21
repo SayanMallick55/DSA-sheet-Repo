7fa371e47d168f2b6ae55a3c75454ed8b8455d99
24051655 <sayanmallick7391@gmail.com>
2026-04-21 23:28:55 +0530

    Changes Made

---
+++ Binary Tress/Pre-order.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    Node(int val){
+        data=val;
+        left=right=NULL;
+    }
+};
+
+void preorder(Node *root){
+    if(root == NULL){
+        return;
+    }
+    cout<<root->data;
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main(){
+
+    return 0;
+}
