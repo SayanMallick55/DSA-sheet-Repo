1187e5e43c32b460db7b0dc7db822610bf958878
24051655 <sayanmallick7391@gmail.com>
2026-04-22 11:04:21 +0530

    Changes Made

--- Binary Tress/Pre-order.cpp
+++ Binary Tress/Pre-order.cpp
@@ -37,3 +37,20 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
+class Solution {
+public:
+    void pre(TreeNode* root,vector<int>&ans){
+        if(root==NULL){
+            return;
+        }
+        ans.push_back(root->val);
+        pre(root->left,ans);
+        pre(root->right,ans);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        pre(root,ans);
+        return ans;
+    }
+};
